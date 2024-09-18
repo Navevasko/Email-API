4fd869d7426364acdef4096363c71b573b7a0370
Guilherme Navevaiko <guilherme_navevaiko@hotmail.com>
2024-09-18 18:02:18 -0300

    fix: retorno após mudanças do banco

--- src/infra/repositories/users/update-user-repository.service.ts
+++ src/infra/repositories/users/update-user-repository.service.ts
@@ -1,5 +1,4 @@
 import type { UpdateUserArgs } from "@domain/models/users/update-user-args";
-import { RequestUser } from "@domain/models/users/user";
 import { PrismaService } from "@main/config/prisma/prisma.service";
 import { Inject, Injectable } from "@nestjs/common";
 import type { User } from "@prisma/client";
@@ -9,26 +8,12 @@ export class UpdateUserRepository {
 	@Inject(PrismaService)
 	private prisma: PrismaService;
 
-	async execute({ id, data }: UpdateUserArgs): Promise<RequestUser> {
+	async execute({ id, data }: UpdateUserArgs): Promise<User> {
 		return await this.prisma.user.update({
 			where: {
 				id,
 			},
 			data,
-			include: {
-				sent_emails: {
-					include: {
-						from: true,
-					},
-				},
-				received_emails: {
-					select: {
-						email: true,
-						is_favorited: true,
-						type: true,
-					},
-				},
-			},
 		});
 	}
 }
