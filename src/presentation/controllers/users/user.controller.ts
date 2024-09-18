5562cd2196586b51c379b311f5a8afbf5506f2dd
Guilherme Navevaiko <guilherme_navevaiko@hotmail.com>
2024-09-18 18:42:24 -0300

    feat: rota para atualização de usuário

--- src/presentation/controllers/users/user.controller.ts
+++ src/presentation/controllers/users/user.controller.ts
@@ -1,9 +1,11 @@
 import { CreateUserUsecase } from "@data/usecases/users/create-user-usecase.service";
 import { FindUserByIdUsecase } from "@data/usecases/users/find-user-by-id-usecase.service";
+import { UpdateUserUsecase } from "@data/usecases/users/update-user-usecase.service";
 import type { HttpResponse } from "@domain/models/base/http";
 import { success } from "@main/utils/api-response";
-import { Body, Controller, Get, Inject, Param, ParseIntPipe, Post } from "@nestjs/common";
-import type { CreateUserDto } from "@presentation/dtos/user/create-user-dto";
+import { Body, Controller, Get, Inject, Param, ParseIntPipe, Patch, Post, UseInterceptors } from "@nestjs/common";
+import { CreateUserDto } from "@presentation/dtos/user/create-user-dto";
+import { UpdateUserDto } from "@presentation/dtos/user/update-user-dto";
 
 @Controller("/users")
 export class UserController {
@@ -22,6 +24,16 @@ export class UserController {
 	@Post()
 	async create(@Body() data: CreateUserDto): Promise<HttpResponse> {
 		await this.createUserUsecase.execute(data);
+
+		return success({});
+	}
+
+	@Inject(UpdateUserUsecase)
+	private updateUserUsecase: UpdateUserUsecase;
+
+	@Patch("/:id")
+	async update(@Param("id", ParseIntPipe) id: number, @Body() data: UpdateUserDto): Promise<HttpResponse> {
+		await this.updateUserUsecase.execute({ id, data });
 
 		return success({});
 	}
