36c972ec9d610c3fb5c1d0524c287730e8e43f56
Guilherme Navevaiko <guilherme_navevaiko@hotmail.com>
2024-09-18 18:41:56 -0300

    fix: exportando updateUserUsecase

--- src/data/usecases/users/user-usecases.module.ts
+++ src/data/usecases/users/user-usecases.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { CreateUserUsecase } from "./create-user-usecase.service";
 import { UserRepositoriesModule } from "@infra/repositories/users/user-repositories.module";
 import { FindUserByIdUsecase } from "./find-user-by-id-usecase.service";
+import { UpdateUserUsecase } from "./update-user-usecase.service";
 
 @Module({
-	providers: [CreateUserUsecase, FindUserByIdUsecase],
-	exports: [CreateUserUsecase, FindUserByIdUsecase],
+	providers: [CreateUserUsecase, FindUserByIdUsecase, UpdateUserUsecase],
+	exports: [CreateUserUsecase, FindUserByIdUsecase, UpdateUserUsecase],
 	imports: [UserRepositoriesModule],
 })
 export class UserUsecases {}
